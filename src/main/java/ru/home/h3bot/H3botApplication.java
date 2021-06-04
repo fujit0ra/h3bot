588135cdc88046b358ec5bce3ea54d3c45a1a106
fujit0ra <araslanovprojects@gmail.com>
2021-06-04 04:33:18 +0400

    main files

---
+++ src/main/java/ru/home/h3bot/H3botApplication.java
@@ -0,0 +1,13 @@
+package ru.home.h3bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H3botApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H3botApplication.class, args);
+    }
+
+}
